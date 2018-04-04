0d21480d3231633d6c290a67daa5615006f2f7b7
Louis SUGY <louis.sugy@insa-lyon.fr>
2018-04-04 23:49:49 +0300

    Minor changes (graphics + removed some comments)

--- src/communication.ts
+++ src/communication.ts
@@ -1,13 +1,4 @@
 export namespace Communication {
-    // class ScoreMessage {
-    //     public messageType: string;
-    //     public score: number;
-
-    //     constructor() {
-    //         this.messageType = "SCORE";
-    //     }
-    // }
-
     export function postScore (newScore: number) {
         var msg = {messageType: "SCORE", score: newScore};
         window.parent.postMessage(msg, "*");
